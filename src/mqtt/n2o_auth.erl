db5cddea1e9e2504b9b32a1a2feceb8797716bb1
Tobias Lindahl <tobias@happihacking.com>
2020-09-14 14:29:31 +0200

    [n2o_auth.erl] Refactor for readability

--- src/mqtt/n2o_auth.erl
+++ src/mqtt/n2o_auth.erl
@@ -3,22 +3,30 @@
 -include("emqttd.hrl").
 -export([init/1, check/3, description/0]).
 
-init([Listeners]) -> {ok, Listeners}.
-get_client_id() -> {_, NPid, _} = emqttd_guid:new(), n2o:to_binary(["emqttd_", integer_to_list(NPid)]).
-description() -> "N2O Authentication Module".
+init([Listeners]) ->
+    {ok, Listeners}.
 
-check(#mqtt_client{client_id = ClientId,
-                    username  = PageModule,
-                    client_pid = ClientPid,
-                    ws_initial_headers = _Headers},
-            _Password, _Listeners) ->
-    ClientId2 = case ClientId of <<>> -> get_client_id(); _ ->  ClientId end,
+description() ->
+    "N2O Authentication Module".
+
+check(#mqtt_client{client_id = ClientId} = Client, _Password, _Listeners) ->
+    ClientId2 = case
+                    ClientId of
+                    <<>> -> get_client_id();
+                    _ ->  ClientId
+                end,
     case ClientId2 of
         <<"emqttd_", _/binary>> ->
-            emqttd_client:subscribe(ClientPid,
-                [{n2o:to_binary(["actions/1/",PageModule,"/",ClientId2]), 2}]),
+            Pid = Client#mqtt_client.client_pid,
+            PageModule = Client#mqtt_client.username,
+            Topic = n2o:to_binary(["actions/1/", PageModule, "/", ClientId2]),
+            emqttd_client:subscribe(Pid, [{Topic, 2}]),
             ignore;
         _ -> ignore
     end;
-check(_Client, _Password, _Opts) -> ignore.
+check(_Client, _Password, _Opts) ->
+    ignore.
 
+get_client_id() ->
+    {_, NPid, _} = emqttd_guid:new(),
+    n2o:to_binary(["emqttd_", integer_to_list(NPid)]).
